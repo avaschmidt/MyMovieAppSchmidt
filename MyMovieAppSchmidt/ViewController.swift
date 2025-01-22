df9e79e9b2eeac75cfc816f9423fe17fcbd80e0e
avaschmidt <ava.schmidt25@d155.org>
2025-01-22 07:17:00 -0600

    Initial Commit

---
+++ MyMovieAppSchmidt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieAppSchmidt
+//
+//  Created by AVA SCHMIDT on 1/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyMovieAppSchmidtTests/MyMovieAppSchmidtTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieAppSchmidtTests.swift
+//  MyMovieAppSchmidtTests
+//
+//  Created by AVA SCHMIDT on 1/22/25.
+//
+
+import Testing
+@testable import MyMovieAppSchmidt
+
+struct MyMovieAppSchmidtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
