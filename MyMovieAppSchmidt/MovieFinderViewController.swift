94b81b812b75270733f766624a08f5e38819b4d2
avaschmidt <ava.schmidt25@d155.org>
2025-01-23 08:04:20 -0600

    Array of Titles Pulled Out

--- MyMovieAppSchmidt/MovieFinderViewController.swift
+++ MyMovieAppSchmidt/MovieFinderViewController.swift
@@ -3,17 +3,27 @@ import UIKit
 
 class MovieFinderViewController: UIViewController {
     
+    @IBOutlet weak var searchBarOutlet: UITextField!
     var movieName = ""
+    var titles = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getMovie()
-    
+        
+        
     }
+    
+    
+    @IBAction func searchAction(_ sender: UIButton) {
+        movieName = searchBarOutlet.text!
+        getMovie()
+    }
+    
+    
     
     func getMovie(){
         let session = URLSession.shared
-        let movieURL = URL(string: "http://www.omdbapi.com/?t=\(movieName)&apikey=f36e3324")!
+        let movieURL = URL(string: "http://www.omdbapi.com/?s=\(movieName)&apikey=f36e3324")!
         let dataTask = session.dataTask(with: movieURL) { data, response, error in
             if let e = error{
                 print("Error! \(e)")
@@ -21,26 +31,37 @@ class MovieFinderViewController: UIViewController {
             else{
                 if let d = data{
                     if let jsonObj = try? JSONSerialization.jsonObject(with: d, options: .fragmentsAllowed) as? NSDictionary{
-                        print(jsonObj)
-                        /* if let year = jsonObj.value(forKey: "Year") as? String{
-                            print("This worked")
-                                print(year)
-                            
-                                DispatchQueue.main.async {
-                                    //self.ghostDateOutlet.text = "Year: \(year)"
-                                }
-                                
-                            } */
-
+                        // print(jsonObj)
+                        
+                        var theSearch = jsonObj["Search"] as! [NSDictionary]
+                        //print(theSearch)
+                        
+                        
+                        /*var title = theSearch[0]["Title"]!
+                         print(title)
+                         
+                         } */
+                        
+                        var i = 0
+                        
+                        for result in theSearch{
+                            var title = theSearch[i]["Title"]!
+                            self.titles.append(title as! String)
+                            i+=1
                         }
+                        //print(self.titles[3])
+                        
+                        
+                        
                     }
                     else{
                         print("Could't get data")
                     }
                 }
             }
+            
+        }
         dataTask.resume()
+        
     }
-    
-
 }
