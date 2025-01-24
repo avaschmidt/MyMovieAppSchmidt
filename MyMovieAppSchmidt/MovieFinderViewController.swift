ff363d1a831374db638958a8dbc03aef60831ba2
avaschmidt <ava.schmidt25@d155.org>
2025-01-24 07:44:27 -0600

    Got data to show on the tableview

--- MyMovieAppSchmidt/MovieFinderViewController.swift
+++ MyMovieAppSchmidt/MovieFinderViewController.swift
@@ -1,22 +1,39 @@
 
 import UIKit
 
-class MovieFinderViewController: UIViewController {
+class MovieFinderViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
+    
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
     @IBOutlet weak var searchBarOutlet: UITextField!
     var movieName = ""
     var titles = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        tableViewOutlet.reloadData()
         
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        cell?.textLabel?.text = "\(titles[indexPath.row])"
+        return cell!
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        tableViewOutlet.reloadData()
+    }
     
     @IBAction func searchAction(_ sender: UIButton) {
         movieName = searchBarOutlet.text!
         getMovie()
+        tableViewOutlet.reloadData()
     }
     
     
